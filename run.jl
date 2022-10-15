66509aca455d2e6d3eb10627defdaa4e7387b7c7
leonardgoeke <lgo@wip.tu-berlin.de>
2022-10-15 19:20:27 +0200

    updated scripts

--- run.jl
+++ run.jl
@@ -1,112 +1,51 @@
 
-<<<<<<< HEAD
-# ! import AnyMOD and packages
+# ! import AnyMOD and options
 
-b = "C:/Users/pacop/.julia/dev/AnyMOD.jl/"
-=======
-# ! string here define scenario, overwrite ARGS with respective values for hard-coding scenarios according to comments
-h = ARGS[1] # resolution of time-series for actual solve, can be 96, 1752, 4392, or 8760
-h_heu = ARGS[2] # resolution of time-series for pre-screening, can be 96, 1752, 4392, or 8760
-grid = ARGS[3] # scenario for grid expansion, can be "_gridExp" and "_noGridExp"
+using Gurobi, AnyMOD
+
+h = ARGS[1]
+res = ARGS[2]
+cyc = ARGS[3]
 t_int = parse(Int,ARGS[4]) # number of threads
 
-obj_str = h * "hours_" * h_heu * "hoursHeu" * grid
-temp_dir = "tempFix_" * obj_str # directory for temporary folder
->>>>>>> 98ce0c599e3d910e891f14494065154c634ced35
 
-using Base.Threads, CSV, Dates, LinearAlgebra, Requires, YAML
-using MathOptInterface, Reexport, Statistics, PyCall, SparseArrays
-using DataFrames, JuMP, Suppressor
-using DelimitedFiles
+using Gurobi, AnyMOD, CSV
 
-<<<<<<< HEAD
-pyimport_conda("networkx","networkx")
-pyimport_conda("matplotlib.pyplot","matplotlib")
-pyimport_conda("plotly","plotly")
-=======
-inputMod_arr = ["_basis",grid,"timeSeries/" * h * "hours_2008_only2040",temp_dir]
-inputHeu_arr = ["_basis",grid,"timeSeries/" * h_heu * "hours_2008_only2040"]
-resultDir_str = "results"
->>>>>>> 98ce0c599e3d910e891f14494065154c634ced35
+t_int = 4
 
-include(b* "src/objects.jl")
-include(b* "src/tools.jl")
-include(b* "src/modelCreation.jl")
-include(b* "src/decomposition.jl")
-
-include(b* "src/optModel/technology.jl")
-include(b* "src/optModel/exchange.jl")
-include(b* "src/optModel/system.jl")
-include(b* "src/optModel/cost.jl")
-include(b* "src/optModel/other.jl")
-include(b* "src/optModel/objective.jl")
-
-include(b* "src/dataHandling/mapping.jl")
-include(b* "src/dataHandling/parameter.jl")
-include(b* "src/dataHandling/readIn.jl")
-include(b* "src/dataHandling/tree.jl")
-include(b* "src/dataHandling/util.jl")
-
-include(b* "src/dataHandling/gurobiTools.jl")
-
-# ! run model
 h = "1752"
+res = "full"
+cyc = "allYearly"
 
-anyM = anyModel(["_basis","timeSeries/" * h * "hours_2008_only2040"],"results", supTsLvl = 2, shortExp = 5, redStep = 1.0, emissionLoss = false)
+scr_str = "h" * h * "_" * res * "_" * cyc
+
+# ! create and run model
+anyM = anyModel(["_basis","_resolution/" * res,"_cyclic/" * cyc,"timeSeries/" * h * "hours_2008_only2040"],"results", supTsLvl = 2, shortExp = 5, redStep = 1.0, emissionLoss = false, objName = scr_str)
 createOptModel!(anyM)
 setObjective!(:cost,anyM)
 
 set_optimizer(anyM.optModel, Gurobi.Optimizer)
 set_optimizer_attribute(anyM.optModel, "Method", 2);
 set_optimizer_attribute(anyM.optModel, "Crossover", 0);
+set_optimizer_attribute(anyM.optModel, "Threads",t_int);
 set_optimizer_attribute(anyM.optModel, "BarConvTol", 1e-5);
 
 optimize!(anyM.optModel)
 
+printIIS(anyM)
+
+# ! report results
 reportResults(:summary,anyM)
 
-<<<<<<< HEAD
-printObject(anyM.parts.tech[:heatpumpAirSpace].cns[:stBal],anyM)
-=======
-include("plottingFiles/formatForPlots.jl")
+# write storage levels
 
-plotSankeyDiagram(anyM,ymlFilter = "plottingFiles/powerAll.yml", name = "powerAll", dropDown = (:timestep,), savaData = true)
-plotSankeyDiagram(anyM,ymlFilter = "plottingFiles/transport.yml", name = "powerToX", dropDown = (:timestep,))
-plotSankeyDiagram(anyM,ymlFilter = "plottingFiles/powerToX.yml", name = "powerToX", dropDown = (:timestep,))
-plotSankeyDiagram(anyM,ymlFilter = "plottingFiles/processHeat.yml", name = "processHeat", dropDown = (:timestep,))
-plotSankeyDiagram(anyM,ymlFilter = "plottingFiles/spaceAndDistrictHeat.yml", name = "spaceAndDistrictHeat", dropDown = (:timestep,))
+stTech_arr = ["lithiumBattery","redoxBattery","heatpumpAirSpace","heatpumpGroundSpace","resistiveHeatSpace","largeWaterTank","pitThermalStorage","h2StorageCavern","h2StorageTank","solarThermalResi_a","solarThermalResi_b","pumpedStorage","reservoir"]
 
-plotSankeyDiagram(anyM,ymlFilter = "plottingFiles/powerFull.yml", name = "powerFull", dropDown = (:timestep,),  rmvNode = ("exchange losses; H2","exchange losses; crude oil","final demand; process heat - low","final demand; process heat - medium","final demand; process heat - high","trade buy; non-solid biomass"))
-plotSankeyDiagram(anyM,ymlFilter = "plottingFiles/h2Full.yml", name = "h2Full", dropDown = (:timestep,), rmvNode = ("final demand; electricity","exchange losses; electricity","exchange losses; crude oil"))
+for st in stTech_arr
 
-#endregion
->>>>>>> 98ce0c599e3d910e891f14494065154c634ced35
-
-tSym = :heatpumpAirSpace
-tInt = sysInt(tSym,anyM.sets[:Te])
-part = anyM.parts.tech[tSym]
-prepTech_dic = prepSys_dic[:Te][tSym]
-
-
-
-inDir = ["_basis","timeSeries/" * h * "hours_2008_only2040"]
-outDir = "results"
-
-objName = ""
-
-csvDelim = ","
-interCapa = :linear
-supTsLvl = 1
-shortExp = 10
-redStep = 1.0
-holdFixed = false
-emissionLoss = true
-forceScr = nothing
-reportLvl = 2
-errCheckLvl = 1
-errWrtLvl = 1
-coefRng = (mat = (1e-2,1e4), rhs = (1e-2,1e2))
-scaFac = (capa = 1e2,  capaStSize = 1e2, insCapa = 1e1,dispConv = 1e3, dispSt = 1e5, dispExc = 1e3, dispTrd = 1e3, costDisp = 1e1, costCapa = 1e2, obj = 1e0)
-bound = (capa = NaN, disp = NaN, obj = NaN)
-avaMin = 0.01
-checkRng = (print = false, all = true)
+    stLvl_df = printObject(anyM.parts.tech[Symbol(st)].var[:stLvl],anyM, rtnDf = (:csvDf,))
+    filter!(x -> x.region_dispatch[end-1:end] == "DE" ,stLvl_df)
+    select!(stLvl_df,[:timestep_dispatch,:variable])
+    stLvl_df = combine(x -> (variable = sum(x.variable)),groupby(stLvl_df,[:timestep_dispatch]))
+    CSV.write("results/stLvl_" * scr_str * "_" * st * ".csv",stLvl_df)
+end
